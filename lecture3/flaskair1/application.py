9c5939b244c89e839bd7ee89e0187db33cd90a0e
Yoimer <ydrf19@gmail.com>
2018-07-08 15:57:45 -0400

    adapted flaskair0 flaskair1 applications and added commands for starting Flask

--- lecture3/flaskair1/application.py
+++ lecture3/flaskair1/application.py
@@ -1,12 +1,23 @@
 import os
-
+import logging
 from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
+'''
 engine = create_engine(os.getenv("DATABASE_URL"))
+this line has to be replaced.
+user has to create a database, a role and a password as shown above
+where:
+user: cs50
+password: 12345
+databasename : c50example
+port: 5432 (default port)
+'''
+
+engine = create_engine("postgresql://cs50:12345@localhost:5432/cs50example")
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
